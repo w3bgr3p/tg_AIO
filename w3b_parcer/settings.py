0bfc19e2d4f2cab862cebf623ed9d84cde3cd635
w3bgr3p <113336090+w3bgr3p@users.noreply.github.com>
2023-10-25 12:09:35 -0500

    Create settings.py

---
+++ w3b_parcer/settings.py
@@ -0,0 +1,9 @@
+# Токен вашего бота, полученный от @BotFather
+BOT_TOKEN = "токен"
+
+# ID вашего канала, где бот является администратором
+MY_CHANNEL_ID = int("-100XXXXXXXXXX")
+
+API_ID = 'XXXXXXXX'
+API_HASH = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+YOUR_USER_ID = 123456789  # Замените 123456789 на ваш реальный ID пользователя в Telegram
